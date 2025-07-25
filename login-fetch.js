6e9f5419f43846b064d69c42f3d136ee51e42eea
schulza <schulzaaa@me.com>
2025-07-25 10:48:05 +0200

    Update login-fetch.js

--- login-fetch.js
+++ login-fetch.js
@@ -8,6 +8,7 @@ const XML_URL = 'https://www.myfxbook.com/feeds/economic-calendar.xml';
 (async () => {
   const browser = await puppeteer.launch({
       headless: 'new',
+      executablePath: '/usr/bin/google-chrome',
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
   const page = await browser.newPage();
